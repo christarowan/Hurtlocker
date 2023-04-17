1258af878ff607a38072758b9e2b9d1939958979
Christa <christa@Zipcoders-MacBook-Pro-6.local>
2023-04-17 11:43:50 -0400

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
 import org.apache.commons.io.IOUtils;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+;
+
 
 public class Main {
 
@@ -7,6 +13,24 @@ public class Main {
         ClassLoader classLoader = getClass().getClassLoader();
         String result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
         return result;
+    }
+
+    public void parseData(String rawData) {
+        String regexPattern = "(?i)naMe:(.*?);price:(.*?);type:(.*?);expiration:(.*?)[##^%*;]";
+        Pattern pattern = Pattern.compile(regexPattern);
+        Matcher matcher = pattern.matcher(rawData);
+
+        while (matcher.find()) {
+            String name = matcher.group(1);
+            String price = matcher.group(2);
+            String type = matcher.group(3);
+            String expiration = matcher.group(4);
+
+            System.out.println("Name: " + name);
+            System.out.println("Price: " + price);
+            System.out.println("Type: " + type);
+            System.out.println("Expiration: " + expiration);
+        }
     }
 
     public static void main(String[] args) throws Exception{
